d550848dc5ad43821f395b6d61704dc97aac5587
willreed111185 <william.reed.11@gmail.com>
2017-10-08 23:02:38 -0400

    commit complete

--- Game.js
+++ Game.js
@@ -1,6 +1,8 @@
 function Game(word) {
   this.word = word;
   this.wordArray = this.word.split("");
+  this.verify = this.word.split("");
+  this.correctGuesses = 0;
   this.displayArray = function(){
   	  let display = [];
 	  	for (i=0;i<this.wordArray.length;i++){
@@ -11,17 +13,23 @@ function Game(word) {
 	}
   this.updateDisplayArray = function(letter,displayArrayCurrent){
   	let test = this.wordArray.indexOf(letter);
-    console.log("TEST: ",test);
   	if(test<0){
-      console.log("You Guessed Wrong");
   		return(0);
   	}
   	while(test>=0){
   		displayArrayCurrent[test] = letter;
   		this.wordArray[test]="!";
+      this.correctGuesses++;
 		  test = this.wordArray.indexOf(letter);
   	}
+    console.log("++++++++++++++++++++++++++++++++++++++++++++++");
+    console.log("++++++++++++++++++++++++++++++++++++++++++++++");
   	console.log(displayArrayCurrent);
+    console.log("++++++++++++++++++++++++++++++++++++++++++++++");
+    console.log("++++++++++++++++++++++++++++++++++++++++++++++");
+    if (this.correctGuesses==this.verify.length){
+      return(1);
+    }
   	return(displayArrayCurrent);
   };
 }
--- hangman.js
+++ hangman.js
@@ -3,12 +3,10 @@ const moment = require("moment-datetime");
 const fs = require("fs");
 const logFilePath = "log.txt";
 const Game = require("./Game");
-const gameWordsArray = ["california","florida","colorado","washington"];
-let gameWord = gameWordsArray[Math.floor(Math.random() * gameWordsArray.length) + 0];
-//console.log("GameWord: ",gameWord)
-var gameWordObj = new Game(gameWord);
+const gameWordsArray = ["california","florida","colorado","washington", "newyork","virginia","georgia","montana"];
+let gameWordObj = {};
+let gameWord="";
 let currentDisplay = [];
-
 
 function logIt(logNote){
 	let timeStamp = moment().strftime("%m/%d/%y %I:%M %p");
@@ -32,11 +30,19 @@ function playHangman(){
 		switch(inquirerResponse.trans){
 			case "No":
 				logIt("Player didnt want to play.");
+				process.exit();
 				break;
 			case "Yes":
+				currentDisplay=[];
+				gameWord = gameWordsArray[Math.floor(Math.random() * gameWordsArray.length) + 0];
+				gameWordObj = new Game(gameWord);
 				logIt("Game Started: "+gameWord);
-				console.log(gameWord);
+				console.log("++++++++++++++++++++++++++++++++++++++++++++++");
+				console.log("++++++++++++++++++++++++++++++++++++++++++++++");
+				//console.log(gameWord);
 				currentDisplay=gameWordObj.displayArray();
+				console.log("++++++++++++++++++++++++++++++++++++++++++++++");
+				console.log("++++++++++++++++++++++++++++++++++++++++++++++");
 				gameRound(6);
 				break;
 			case "Maybe":
@@ -48,19 +54,52 @@ function playHangman(){
 
 function gameRound(tries){
 	if(tries==0){
-		return
+		console.log("==================");
+		console.log("==================");
+		console.log("You Lost That Game");
+		console.log("==================");
+		console.log("==================");
+		logIt("GameOver: ",gameWordObj.word);
+		playHangman();
+		return;
 	};
 	inquirer.prompt([
 	    {
 	      type: "input",
-	      message: "Please Guess a letter",
+	      message: "Please Guess a letter:",
 	      name: "letterGuess"
 	    }
 	]).then(function(inquirerResponse) {
 		let turn = gameWordObj.updateDisplayArray(inquirerResponse.letterGuess,currentDisplay);
 		console.log("You guessed: "+inquirerResponse.letterGuess);
 		if (turn==0){
-			tries--;
+			let test = gameWordObj.verify.indexOf(inquirerResponse.letterGuess);
+			if (test==-1){
+				tries--;
+				console.log("====================================");
+				console.log("====================================");
+				console.log("You Guessed Wrong: Only "+ tries + " Tries Left");
+				console.log("====================================");
+				console.log("====================================");
+				logIt("Wrong Guess: "+inquirerResponse.letterGuess);
+			}else{
+				console.log("========================");
+				console.log("========================");
+				console.log("You Already Guessed That");
+				console.log("========================");
+				console.log("========================");
+				logIt("Repeat Guess: "+inquirerResponse.letterGuess);
+			}
+		}
+		else if (turn==1){
+			console.log("==================");
+			console.log("==================");
+			console.log("You Win That Round");
+			console.log("==================");
+			console.log("==================");
+			logIt("Round Won");
+			playHangman();
+			return;
 		}else{
 		currentDisplay = turn;	
 		}
@@ -69,14 +108,4 @@ function gameRound(tries){
 }
 
 playHangman();
-
-
-
-
-//console.log(gameWordObj.word);
-//console.log(gameWordObj.wordArray);
-
-//var output = gameWordObj.displayArray();
-//var gameTurn = gameWordObj.updateDisplayArray("o",output);
-//console.log(gameTurn);
 
